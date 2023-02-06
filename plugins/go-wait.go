d20ef21116451cfca8f646f27df5c6fdd1116754
thanhnguyen <chithanh12@gmail.com>
2023-02-06 18:20:56 +0700

    remove panic

--- plugins/go-wait.go
+++ plugins/go-wait.go
@@ -27,7 +27,7 @@ func (config Config) Access(kong *pdk.PDK) {
 
 	host, _ := kong.Request.GetHost()
 	lastRequest, exists := requests[host]
-	panic("xxx")
+
 	if exists && time.Now().Sub(lastRequest) < time.Duration(config.WaitTime)*time.Second {
 		kong.Response.Exit(400, "Maximum Requests Reached", make(map[string][]string))
 	} else {
